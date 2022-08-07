5dbe611ac761918391c0d6777d0c857781a74e3f
mehulsatardekar <satardekar042197@gmail.com>
2022-08-07 13:57:32 +0530

    feat : formik, supabase with not found component and barrel exporter added

---
+++ src/UI/Pages/index.js
@@ -0,0 +1,10 @@
+export { HistoryPage } from "./HistoryPage/HistoryPage";
+export { HomePage } from "./HomePage/HomePage";
+export { LikeVideoPage } from "./LikeVideoPage/LikeVideoPage";
+export { PlaylistMoviesPage } from "./PlaylistPage/PlaylistMoviesPage";
+export { PlaylistPage } from "./PlaylistPage/PlaylistPage";
+export { SingleVideoPage } from "./SingleVideoPage/SingleVideoPage";
+export { VideoListingPage } from "./VideoListingPage/VideoListingPage";
+export { WatchLaterPage } from "./WatchLaterPage/WatchLaterPage";
+export { LoginPage } from "./LoginPage/LoginPage";
+export { SignupPage } from "./SignupPage/SignupPage";
