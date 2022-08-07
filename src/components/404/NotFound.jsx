44182a35bebcecbff35e2d705a0f366f6811170e
mehulsatardekar <satardekar042197@gmail.com>
2022-08-07 14:08:00 +0530

    fix: fixed 404 image scaling issue on small devices

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
       <section className="flex flex-column flex-justify-center flex-align-item-center">
         <img
           className="page-404"
-          src="https://res.cloudinary.com/dwhsfh3sc/image/upload/v1649259193/Offers-imgs/page404_gsf4ar.png"
+          src="https://images.weserv.nl/?url=https://res.cloudinary.com/dwhsfh3sc/image/upload/v1659861109/zeplin-movies/movie-assets-imgs/page404_gsf4ar-removebg-preview_ehg4mx.png"
           alt="Not-found-404"
         />
         <h2>Oops.. page not found</h2>
